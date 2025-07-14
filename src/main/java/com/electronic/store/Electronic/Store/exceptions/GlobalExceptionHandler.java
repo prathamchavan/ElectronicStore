7023db7d32e94d2f9ac9b31d7b84cd891ebb712f
Prathamesh Chavan <prathamchavan0013@gmail.com>
2025-07-15 01:00:52 +0530

    Creating image upload service and throwing BadApiRequest

--- src/main/java/com/electronic/store/Electronic/Store/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/electronic/store/Electronic/Store/exceptions/GlobalExceptionHandler.java
@@ -45,4 +45,13 @@ public class GlobalExceptionHandler {
         });
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+
+    //handle bad api exception
+    @ExceptionHandler(BadApiRequest.class)
+    public ResponseEntity<ApiResponseMessage> handleBadApiRequest(BadApiRequest ex)
+    {
+        logger.info("Bad Api Request!!");
+        ApiResponseMessage response = ApiResponseMessage.builder().message(ex.getMessage()).status(HttpStatus.BAD_REQUEST).success(false).build();
+        return new ResponseEntity(response,HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/com/electronic/store/Electronic/Store/services/FileService.java
+++ src/main/java/com/electronic/store/Electronic/Store/services/FileService.java
@@ -1,4 +1,14 @@
 package com.electronic.store.Electronic.Store.services;
 
-public class FileService {
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface FileService {
+
+    String uploadImage(MultipartFile file, String path) throws IOException;
+
+    InputStream getResource(String path, String name) throws FileNotFoundException;
 }
